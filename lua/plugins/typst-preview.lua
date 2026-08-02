14e4a900d90beabc32650af839de48936387ade2
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2026-08-02 11:41:56 +0800

    update dep

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,11 +1,10 @@
 return {
-	-- 'chomosuke/typst-preview.nvim',
-	-- Partial rendering fork
-	'll-nick/typst-preview.nvim',
-	branch = 'partial-rendering-default',
+	'chomosuke/typst-preview.nvim',
 	ft = 'typst',
 	opts = {
-		dependencies_bin = { tinymist = 'tinymist.cmd' },
+		dependencies_bin = {
+			tinymist = (vim.fn.has('win32') ~= 0) and 'tinymist.cmd' or 'tinymist',
+		},
 		get_main_file = function() return vim.fn.getcwd() .. '/main.typ' end,
 	},
 }
